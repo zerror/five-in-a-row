9de7ab0f2a4a2c7cf50c980aa2f2234afd7f3e85
anteroh <antero.hynynen@gmail.com>
2019-03-03 14:09:23 +0200

    Added red color to O marks on board.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+  	<button className={props.value ? "square " + props.value : "square"} onClick={props.onClick}>
       {props.value}
     </button>
   );
