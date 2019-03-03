e8ebeacf0dd874c6e658c65e8edb7481040b05d5
anteroh <antero.hynynen@gmail.com>
2019-03-03 12:27:10 +0200

    Changed rules so that winning now requires 5 marks in a line. Added history array. Moved board properties to game class. Reset button top margin added and board is now 15 * 15.

--- src/index.js
+++ src/index.js
@@ -11,33 +11,59 @@ function Square(props) {
 }
 
 class Board extends React.Component {
+  renderSquare(i) {
+  	return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+  render() {
+
+    var rows = [];
+	  for (let i = 0; i < this.props.cols; i++) {
+	    let cells = []
+	    for (let j = 0; j < this.props.cols; j++) {
+        cells.push(this.renderSquare(i * this.props.cols + j));
+	    }
+      rows.push(<div className="board-row" key={i}>{cells}</div>);
+    }
+
+    return (
+      <div>
+        {rows}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.cols = 10;
+    this.cols = 15;
     this.state = {
-      squares: Array(this.cols * this.cols).fill(null),
+      history: [{
+        squares: Array(this.cols * this.cols).fill(null),
+      }],
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-  	const squares = this.state.squares.slice();
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
     if (calculateWinner(squares, this.cols) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-		squares: squares,
-		xIsNext: !this.state.xIsNext,
-	});
-  }
-
-  renderSquare(i) {
-  	return <Square key={i} value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+      history: history.concat([{ squares: squares, }]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares, this.cols);
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares, this.cols);
     let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 	  if (winner) {
       status = 'Winner: ' + winner;
@@ -45,39 +71,22 @@ class Board extends React.Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
-    var rows = [];
-	  for (let i = 0; i < this.cols; i++) {
-	    let cells = []
-	    for (let j = 0; j < this.cols; j++) {
-        cells.push(this.renderSquare(i * this.cols + j));
-	    }
-      rows.push(<div className="board-row" key={i}>{cells}</div>);
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
-        {rows}
-        <input type="button" onClick={() => this.setState({
-          squares: Array(this.cols * this.cols).fill(null),
-          xIsNext: true,
-        })} value="Restart game" />
-      </div>
-    );
-  }
-}
+        <div className="game">
+          <div className="game-board">
+            <Board squares={current.squares} cols={this.cols} onClick={(i) => this.handleClick(i)}/>
+          </div>
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+          <div className="game-info">
+            <div className="status">{status}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+        <input className="reset-button" type="button" onClick={() => this.setState({
+          history: [{ squares: Array(this.cols * this.cols).fill(null) }],
+          xIsNext: this.state.xIsNext,
+        })} value="Restart game" />
       </div>
     );
   }
@@ -94,17 +103,26 @@ function calculateWinner(squares, cols) {
     let fitsInCol = i + cols * 3 <= squares.length;
     let fitsInDownDiagonal = i + cols * 3 + 3 <= squares.length;
     let fitsInUpDiagonal = i + cols * 3 - 3 <= squares.length;
-    if (fitsInRow && squares[i + 1] === mark && squares[i + 2] === mark && squares[i + 3] === mark) {
-      return mark;
+
+    if (fitsInRow || fitsInCol){
+      if (squares[i + 1] === mark && squares[i + 2] === mark && squares[i + 3] === mark && squares[i + 4] === mark) {
+        return mark;
+      }
+      if (squares[i + cols] === mark && squares[i + cols * 2] === mark && squares[i + cols * 3] === mark && squares[i + cols * 4] === mark) {
+        return mark;
+      }
     }
-    if (fitsInCol && squares[i + cols] === mark && squares[i + cols * 2] === mark && squares[i + cols * 3] === mark) {
-      return mark;
+    if (fitsInDownDiagonal) {
+      for (let x = 1; x < 5; x++) {
+        if (squares[i + cols * x + x] !== mark) { break; }
+        if (x === 4) { return mark; }
+      }
     }
-    if (fitsInDownDiagonal && squares[i + cols + 1] === mark && squares[i + cols * 2 + 2] === mark && squares[i + cols * 3 + 3] === mark) {
-      return mark;
-    }
-    if (fitsInUpDiagonal && squares[i + cols - 1] === mark && squares[i + cols * 2 - 2] === mark && squares[i + cols * 3 - 3] === mark) {
-      return mark;
+    if (fitsInUpDiagonal) {
+      for (let x = 1; x < 5; x++) {
+        if (squares[i + cols * x - x] !== mark) { break; }
+        if (x === 4) { return mark; }
+      }
     }
   }
 
