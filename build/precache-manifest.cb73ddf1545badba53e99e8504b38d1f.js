9cedbf8be3f033c273d459bc7d78456a919ce420
anteroh <antero.hynynen@gmail.com>
2019-04-16 01:50:19 +0300

    Build done locally to run react client and socket IO server in one heroku instance

---
+++ build/precache-manifest.cb73ddf1545badba53e99e8504b38d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "70f872a363b36e1663d5",
+    "url": "/static/js/main.c91680d2.chunk.js"
+  },
+  {
+    "revision": "90ee3b23438ed088080e",
+    "url": "/static/js/2.7a42e94e.chunk.js"
+  },
+  {
+    "revision": "70f872a363b36e1663d5",
+    "url": "/static/css/main.86f27643.chunk.css"
+  },
+  {
+    "revision": "7c466495aa65bda20e89bc9cda762e36",
+    "url": "/index.html"
+  }
+];
